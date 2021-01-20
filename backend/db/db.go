f60b5a0583928015be0f0e40a23ff4e58f2ff868
Paulo <pauloeduardo2709@gmail.com>
2021-01-20 19:05:37 -0300

    Add admin user creation

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/Pauloo27/shop/models"
+	"github.com/Pauloo27/shop/utils"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -19,6 +22,28 @@ func Connect() error {
 
 	// migrate models
 	Database.AutoMigrate(&models.User{})
+
+	// create default admin user
+	var user models.User
+	Database.First(&user, 1)
+
+	if user.ID != 1 {
+		// random word as password
+		defaultPassword := "atmosfera"
+
+		fmt.Println("Default user not found, creating one...")
+		user = models.User{
+			Name: "admin", IsAdmin: true,
+			Password: utils.HashPassword(defaultPassword),
+		}
+
+		err = Database.Create(&user).Error
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Default user is admin:%s\n", defaultPassword)
+	}
 
 	return nil
 }
