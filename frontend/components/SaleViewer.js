c13366dc2570c089074f18fb52246967d3e377d3
Paulo <pauloeduardo2709@gmail.com>
2021-01-21 20:44:50 -0300

    Format SaleViewer

--- frontend/components/SaleViewer.js
+++ frontend/components/SaleViewer.js
@@ -1,3 +1,13 @@
-export default function SaleViewer({sale}) {
-  return <h3>{sale.ID}</h3>
+export default function SaleViewer({ sale }) {
+  const date = sale.CreatedAt.substring(0, 10);
+  const time = sale.CreatedAt.substring(11, 16);
+  return (
+    <h4>
+      <span className="text-primary">{sale.User.Name}</span> vendeu um{" "}
+      <span className="text-primary">{sale.Product.Name} </span>
+      por <span className="text-primary">R${sale.Product.Price}</span> dia{" "}
+      <span className="text-primary">{date}</span> às{" "}
+      <span className="text-primary">{time}</span>.
+    </h4>
+  );
 }
