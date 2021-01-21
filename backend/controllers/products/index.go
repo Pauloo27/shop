07bf37b2115a64e8cea36035b28c11ef41b2f263
Paulo <pauloeduardo2709@gmail.com>
2021-01-21 17:28:02 -0300

    Add last page to response

--- backend/controllers/products/index.go
+++ backend/controllers/products/index.go
@@ -1,12 +1,15 @@
 package products
 
 import (
+	"errors"
+	"math"
 	"strconv"
 
 	"github.com/Pauloo27/shop/db"
 	"github.com/Pauloo27/shop/models"
 	"github.com/Pauloo27/shop/utils"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 const PAGE_SIZE = 10
@@ -17,11 +20,21 @@ func Index(c *fiber.Ctx) error {
 		return utils.AsError(c, fiber.StatusBadRequest, "Página inválida")
 	}
 
+	var lastProduct models.Product
+	err = db.Database.Last(&lastProduct).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return utils.AsError(c, fiber.StatusNotFound, "Nenhum produto cadastrado")
+		}
+		panic(err)
+	}
+
 	var products []models.Product
 	err = db.Database.Where("id > ?", PAGE_SIZE*(page-1)).Limit(PAGE_SIZE).Find(&products).Error
 	if err != nil {
 		panic(err)
 	}
 
-	return c.JSON(products)
+	lastPage := int(math.Ceil(float64(lastProduct.ID) / float64(PAGE_SIZE)))
+	return c.JSON(fiber.Map{"products": products, "last_page": lastPage})
 }
--- backend/controllers/sales/index.go
+++ backend/controllers/sales/index.go
@@ -1,12 +1,15 @@
 package sales
 
 import (
+	"errors"
+	"math"
 	"strconv"
 
 	"github.com/Pauloo27/shop/db"
 	"github.com/Pauloo27/shop/models"
 	"github.com/Pauloo27/shop/utils"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 const PAGE_SIZE = 10
@@ -17,6 +20,15 @@ func Index(c *fiber.Ctx) error {
 		return utils.AsError(c, fiber.StatusBadRequest, "Página inválida")
 	}
 
+	var lastSale models.Sale
+	err = db.Database.Last(&lastSale).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return utils.AsError(c, fiber.StatusNotFound, "Nenhuma venda cadastrada")
+		}
+		panic(err)
+	}
+
 	var sales []models.Sale
 	err = db.Database.Preload("Product").Where("id > ?", PAGE_SIZE*(page-1)).
 		Limit(PAGE_SIZE).Find(&sales).Error
@@ -24,5 +36,6 @@ func Index(c *fiber.Ctx) error {
 		panic(err)
 	}
 
-	return c.JSON(sales)
+	lastPage := int(math.Ceil(float64(lastSale.ID) / float64(PAGE_SIZE)))
+	return c.JSON(fiber.Map{"sales": sales, "last_page": lastPage})
 }
