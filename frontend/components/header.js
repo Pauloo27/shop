a7d6624d99c7c9a4889ed900feedf429986b5df5
Paulo <pauloeduardo2709@gmail.com>
2021-01-21 14:31:51 -0300

    Add header content

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Ínicio
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  );
 }
