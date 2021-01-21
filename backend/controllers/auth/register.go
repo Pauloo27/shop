31f245a893b24a8d65f4f4c0b06deac39ebd2cb8
Paulo <pauloeduardo2709@gmail.com>
2021-01-20 23:21:43 -0300

    Add register

--- backend/controllers/auth/register.go
+++ backend/controllers/auth/register.go
@@ -1,7 +1,39 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Pauloo27/shop/db"
+	"github.com/Pauloo27/shop/models"
+	"github.com/Pauloo27/shop/utils"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Register(c *fiber.Ctx) error {
-	return c.SendString("asd")
+	payload := new(AuthPayload)
+
+	if err := c.BodyParser(payload); err != nil {
+		return c.SendStatus(fiber.ErrBadRequest.Code)
+	}
+
+	errs := utils.Validate(payload)
+	if errs != nil {
+		return c.JSON(errs)
+	}
+
+	newUser := models.User{
+		Name:        payload.Name,
+		Password:    utils.HashPassword(payload.Password),
+		CreatedByID: c.Locals("user_id").(int),
+	}
+
+	err := db.Database.Create(&newUser).Error
+	if err != nil {
+		// FIXME
+		// There's no gorm.ErrUnique... so... raw string check?
+		if err.Error() == "UNIQUE constraint failed: users.name" {
+			return utils.AsError(c, fiber.StatusConflict, "Nome já em uso")
+		}
+		panic(err)
+	}
+
+	return utils.AsJSON(c, fiber.StatusCreated, fiber.Map{"id": newUser.ID})
 }
