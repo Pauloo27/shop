b7f9560499aa9c1a4b289093eae2ce3fbcec4529
Paulo <pauloeduardo2709@gmail.com>
2021-01-21 17:13:57 -0300

    Add simple pagination

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,6 +5,7 @@ import API from "../services/API";
 export default function Home() {
   const [products, setProducts] = useState(undefined);
   const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
 
   useEffect(() => {
     setProducts(undefined);
@@ -18,14 +19,40 @@ export default function Home() {
   const showProducts = useCallback(() => {
     if (products === undefined) return <span>Carregando...</span>;
     return products.map((product) => (
-      <ProductViewer key={product.ID} product={product} sell={true} edit={false} />
+      <ProductViewer
+        key={product.ID}
+        product={product}
+        sell={true}
+        edit={false}
+      />
     ));
   }, [products]);
+
+  const listPages = useCallback(() => {
+    const pages = [];
+    for(let i = 1; i <= lastPage; i++) {
+      pages.push(i);
+    }
+    return pages.map(i => {
+      return <li class="page-item"><button class="page-link">{i}</button></li>
+    });
+  }, [page, lastPage]);
 
   return (
     <div className="container">
       <h3>Produtos</h3>
       {showProducts()}
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <button className="page-link">&laquo;</button>
+          </li>
+          {listPages()}
+          <li className="page-item">
+            <button className="page-link">&raquo;</button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
