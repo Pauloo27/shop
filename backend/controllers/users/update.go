e83d830c03a9f4968d981851420286d08e693438
Paulo <pauloeduardo2709@gmail.com>
2021-01-20 23:50:13 -0300

    Add users route to update their password

---
+++ backend/controllers/users/update.go
@@ -0,0 +1,50 @@
+package users
+
+import (
+	"github.com/Pauloo27/shop/db"
+	"github.com/Pauloo27/shop/models"
+	"github.com/Pauloo27/shop/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+type UpdatePayload struct {
+	CurrentPassword         string `validate:"required,min=5,max=32"`
+	NewPassword             string `validate:"required,min=5,max=32"`
+	NewPasswordConfirmation string `validate:"required,min=5,max=32"`
+}
+
+func Update(c *fiber.Ctx) error {
+	payload := new(UpdatePayload)
+
+	if err := c.BodyParser(payload); err != nil {
+		return c.SendStatus(fiber.ErrBadRequest.Code)
+	}
+
+	errs := utils.Validate(payload)
+	if errs != nil {
+		return c.JSON(errs)
+	}
+
+	var user models.User
+	err := db.Database.First(&user, c.Locals("user_id").(int)).Error
+	if err != nil {
+		panic(err)
+	}
+
+	if user.Password != utils.HashPassword(payload.CurrentPassword) {
+		return utils.AsError(c, fiber.StatusForbidden, "Senha incorreta")
+	}
+
+	if payload.NewPassword != payload.NewPasswordConfirmation {
+		return utils.AsError(c, fiber.StatusForbidden, "Confirmação de senha inválida")
+	}
+
+	err = db.Database.Model(user).Update("Password",
+		utils.HashPassword(payload.NewPassword),
+	).Error
+
+	if err != nil {
+		panic(err)
+	}
+	return utils.AsMsg(c, fiber.StatusAccepted, "Senha alterada")
+}
--- backend/router/router.go
+++ backend/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/Pauloo27/shop/controllers/auth"
+	"github.com/Pauloo27/shop/controllers/users"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,4 +10,8 @@ func RouteFor(app *fiber.App) {
 	// Auth
 	app.Post("/v1/login", requireGuest, auth.Login)
 	app.Post("/v1/register", requireAuth, requireAdmin, auth.Register)
+
+	// User
+	app.Put("/v1/users/", requireAuth, users.Update)
+	//app.Put("/v1/users/:id", requireAuth, requireAdmin, users.UpdateOther)
 }
