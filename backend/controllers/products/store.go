7a3cc921fe7ba07b71f03c8f191e22bf60bdac7e
Paulo <pauloeduardo2709@gmail.com>
2021-01-21 11:23:22 -0300

    Add products.Store()

---
+++ backend/controllers/products/store.go
@@ -0,0 +1,42 @@
+package products
+
+import (
+	"github.com/Pauloo27/shop/db"
+	"github.com/Pauloo27/shop/models"
+	"github.com/Pauloo27/shop/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+type StorePayload struct {
+	Name  string  `validate:"required,min=3,max=32"`
+	Price float32 `validate:"required,min=0"`
+}
+
+func Store(c *fiber.Ctx) error {
+	payload := new(StorePayload)
+
+	if err := c.BodyParser(payload); err != nil {
+		return c.SendStatus(fiber.ErrBadRequest.Code)
+	}
+
+	errs := utils.Validate(payload)
+	if errs != nil {
+		return c.JSON(errs)
+	}
+
+	product := models.Product{
+		Name:   payload.Name,
+		Price:  payload.Price,
+		Amount: 0,
+	}
+
+	err := db.Database.Create(&product).Error
+	if err != nil {
+		if utils.IsNotUnique(err) {
+			return utils.AsError(c, fiber.StatusConflict, "Um produto com esse nome já existe")
+		}
+		panic(err)
+	}
+
+	return utils.AsJSON(c, fiber.StatusCreated, fiber.Map{"id": product.ID})
+}
--- backend/router/router.go
+++ backend/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/Pauloo27/shop/controllers/auth"
+	"github.com/Pauloo27/shop/controllers/products"
 	"github.com/Pauloo27/shop/controllers/users"
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,4 +15,7 @@ func RouteFor(app *fiber.App) {
 	// User
 	app.Put("/v1/users/", requireAuth, users.Update)
 	app.Put("/v1/users/:id", requireAuth, requireAdmin, users.UpdateOther)
+
+	// Product
+	app.Post("/v1/products/", requireAuth, requireAdmin, products.Store)
 }
