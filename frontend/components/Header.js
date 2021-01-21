c0f9d07cdff9f7181c7b42506595bc13ded59efe
Paulo <pauloeduardo2709@gmail.com>
2021-01-21 18:47:11 -0300

    Add links to header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -6,6 +6,36 @@ export default function Header() {
       <Link href="/">
         <span className="navbar-brand">Ínicio</span>
       </Link>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Gerenciar
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <Link href="/products/">
+                <span className="dropdown-item">Produtos</span>
+              </Link>
+              <Link href="/sales/">
+                <span className="dropdown-item">Vendas</span>
+              </Link>
+              <Link href="/users/">
+                <span className="dropdown-item">Usuários</span>
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </div>
+
       <button
         className="navbar-toggler"
         type="button"
