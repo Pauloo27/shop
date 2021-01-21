6c68414adb687f69b318a9cfa3ebde989a690888
Paulo <pauloeduardo2709@gmail.com>
2021-01-20 21:50:32 -0300

    Add user name and is_admin to locals

--- backend/router/helper.go
+++ backend/router/helper.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/Pauloo27/shop/utils"
+	jwt "github.com/form3tech-oss/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,6 +12,10 @@ func isAuthed(c *fiber.Ctx) bool {
 
 func requireAuth(c *fiber.Ctx) error {
 	if isAuthed(c) {
+		user := c.Locals("user").(*jwt.Token)
+		claims := user.Claims.(jwt.MapClaims)
+		c.Locals("user_name", claims["name"].(string))
+		c.Locals("user_admin", claims["admin"].(bool))
 		return c.Next()
 	}
 	return utils.AsError(c, fiber.StatusForbidden, "Não autenticado")
